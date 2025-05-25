1dcb9a44a3ce0bf62168519ee1adc1b65620e855
aqzsha <hepokz008@gmail.com>
2025-05-26 03:57:36 +0500

    feat #13-14: session

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import routes from './routes/index.js';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import { mockUsers } from './utils/constants.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser('helloworld'));
+app.use(
+  session({
+    secret: 'aqzsha',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  })
+);
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
@@ -15,6 +27,29 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (request, response) => {
+  console.log(request.session);
+  console.log(request.session.id);
+  request.session.visited = true;
   response.cookie('hello', 'world', { maxAge: 30000, signed: true });
   response.status(201).send({ msg: 'Hello!' });
+});
+
+app.post('/api/auth', (request, response) => {
+  const {
+    body: { username, password },
+  } = request;
+
+  const findUser = mockUsers.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password) {
+    return response.status(401).send({ msg: 'BAD CREDENTIALS' });
+  }
+
+  request.session.user = findUser;
+  return response.status(200).send(findUser);
+});
+
+app.get('/api/auth/status', (request, response) => {
+  return request.session.user
+    ? response.status(200).send(request.session.user)
+    : response.status(401).send({ msg: 'Not Authenticated' });
 });
