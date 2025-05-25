f8f95962e21e5025c5af6b6b1f8fa9f40b0eece7
aqzsha <hepokz008@gmail.com>
2025-05-26 03:21:01 +0500

    feat #12: cookies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import routes from './routes/index.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser('helloworld'));
 app.use(routes);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
+});
+
+app.get('/', (request, response) => {
+  response.cookie('hello', 'world', { maxAge: 30000, signed: true });
+  response.status(201).send({ msg: 'Hello!' });
 });
